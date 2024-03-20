4e20ba77d33e0ee7230f4be635f02672eb735aff
Ixyz.dev <ixyz.dev@gmail.com>
2024-03-20 00:58:47 -0300

    Paleta de colores

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body className={inter.className}>
-        <main className="min-h-screen">
+        <main className="min-h-screen bg-primary">
           <NavBar />
           {children}
           <Footer />
